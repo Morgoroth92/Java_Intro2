c1bebf94557d362aadbd3c8a055cce244a3be499
Claudio V. Carta <claudio.vittorio.carta@gmail.com>
2023-12-19 11:26:47 +0100

    Initial commit

---
+++ Java_Intro2/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        char A = 'a';
+        char B = 'b';
+        String combinazione = "" + A + B;
+        System.out.println(combinazione);
+    }
+
+}
